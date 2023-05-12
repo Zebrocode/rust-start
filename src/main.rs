529dcbda39919d189ea8d584beb2dbfdf175858b
ysx <yangshangxupro@gmail.com>
2023-05-12 17:53:17 +0800

    c

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@
 
 
 fn main() {
-    let s = String::from("hello");
-    take_reference(&s);
+    let s: String = String::from("hello world");
+    println!("{}", first_word(&s));
 
-    // 多个可变引用 引用同一个值 错
-    // 可变引用和不可变引用 引用同一个值 错
-    // 唯一允许的情况是多个不可变引用引用同一个值
-    let s1 = &s;
-    let s2 = &s;
-
-    take_reference(s1);
-    take_reference(s2);
-
-    // 编译器决定不会产生悬垂引用,也就是说引用的作用域内,值一定是有效的,没有被销毁的
+    let a: [i32; 5] = [1,2,3,4,5];
+    let b: &[i32] = &a[..2];
 }
 
-fn take_reference(s : &String) -> usize{
-    // wrong, write s
-    // s.push_str("xxx");
+fn first_word(s : &String) -> usize{
+    let bytes = s.as_bytes();
+    for (i, &c) in bytes.iter().enumerate() {
+        if c == b' ' {
+            return i;
+        }
+    }
+    return bytes.len();
+}
 
-    // yes, read s
-    s.len()
+// best practice
+fn first_word_slice(s : &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &c) in bytes.iter().enumerate() {
+        if c == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
